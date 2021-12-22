db92a20bedfba66149a8623d0ddbca981b07a70b
timhealz <healyt22@gmail.com>
2021-12-22 11:42:50 -0800

    moves from gradle to maven

---
+++ solvers/src/main/java/com/timhealz/cs/algorithms/DynamicConnectivity.java
@@ -0,0 +1,32 @@
+package com.timhealz.cs.algorithms.graph.unionfind;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.timhealz.cs.algorithms.graph.unionfind.QuickFind;
+
+public class DynamicConnectivity {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner in = new Scanner(new File(args[0]));
+
+        int N = in.nextInt();
+
+        QuickFind quickFind = new QuickFind(N);
+
+        while (in.hasNextInt()) {
+            int p = in.nextInt();
+            int q = in.nextInt();
+
+            if (quickFind.connected(p, q)) continue;
+
+            quickFind.union(p, q);
+            System.out.println(p + " " + q);
+        }
+        
+        System.out.println(quickFind.count() + " components");
+
+        in.close();
+    }
+}
