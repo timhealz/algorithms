c77a79a6dba84103a6984e719d94eb33e217c559
timhealz <healyt22@gmail.com>
2021-12-24 12:41:37 -0800

    implements both union find algorithms

--- core/src/main/java/com/timhealz/algorithms/graph/unionfind/QuickUnion.java
+++ core/src/main/java/com/timhealz/algorithms/graph/unionfind/QuickUnion.java
@@ -17,6 +17,7 @@ public class QuickUnion extends AbstractUnionFind {
 
     public int find(int p) {
         while (p != id[p]) {
+            id[p] = id[id[p]];
             p = id[p];
         }
 
--- problems/src/main/java/com/timhealz/algorithms/problems/DynamicConnectivity.java
+++ problems/src/main/java/com/timhealz/algorithms/problems/DynamicConnectivity.java
@@ -3,30 +3,70 @@ package com.timhealz.algorithms.problems;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.UnsupportedOperationException;
 
 import com.timhealz.algorithms.graph.unionfind.QuickFind;
+import com.timhealz.algorithms.graph.unionfind.QuickUnion;
+import com.timhealz.algorithms.graph.unionfind.AbstractUnionFind;
+
+
+enum Algorithms {
+    QUICK_FIND,
+    QUICK_UNION
+}
 
 public class DynamicConnectivity {
-    public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner in = new Scanner(new File(args[0]));
+    private Algorithms algorithm;
+    private AbstractUnionFind unionFind;
+
+    public DynamicConnectivity(Algorithms algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public void run(String filename) throws FileNotFoundException {
+
+        Scanner in = new Scanner(new File(filename));
 
         int N = in.nextInt();
 
-        QuickFind quickFind = new QuickFind(N);
+        switch (this.algorithm) {
+            case QUICK_FIND: 
+                System.out.println("Running with Quick Find.");
+                unionFind = new QuickFind(N);
+                break;
+
+            case QUICK_UNION: 
+                System.out.println("Running with Quick Union.");
+                unionFind = new QuickUnion(N);
+                break;
+
+            default:
+                throw new UnsupportedOperationException("No algorithm provided!");
+        }
 
         while (in.hasNextInt()) {
             int p = in.nextInt();
             int q = in.nextInt();
 
-            if (quickFind.connected(p, q)) continue;
+            if (unionFind.connected(p, q)) continue;
 
-            quickFind.union(p, q);
+            unionFind.union(p, q);
             System.out.println(p + " " + q);
         }
         
-        System.out.println(quickFind.count() + " components");
+        System.out.println(unionFind.count() + " components");
 
         in.close();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        DynamicConnectivity dynConn1 = new DynamicConnectivity(Algorithms.QUICK_FIND);
+        dynConn1.run(args[0]);
+
+        DynamicConnectivity dynConn2 = new DynamicConnectivity(Algorithms.QUICK_UNION);
+        dynConn2.run(args[0]);
+
     }
 }
